f47ad9aa7d8ed1dcc6bb1d6246098ded965c1af9
perkles <deniordqr@hotmail.com>
2022-05-09 20:01:36 -0300

    Add project files.

---
+++ TrainingNineteen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good Morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+
+            /*DateTime dateTime = new DateTime (2022, 04, 29, 8, 25, 54);
+            Console.WriteLine(dateTime.ElapsedTime());*/
+
+
+        }
+    }
+}
